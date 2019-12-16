aae0a20ff98496532ff9eff1ea2ec33130e709fa
Hyung <13234540727@163.com>
2019-12-16 14:00:25 +0800

    改bug

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/warehouse-management/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
   // 跨域
   devServer: {
     proxy: {
